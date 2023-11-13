3e0f7bc9c566aca2bdc4119e56b5b9eb3e254a0f
Kevin DA-RE <dare.kevin21000@gmail.com>
2023-11-13 23:44:58 +0100

    mise en place formulaire

--- functions.php
+++ functions.php
@@ -18,4 +18,34 @@ function nav_menu (string $linkClass = ''): string
     nav_item('/index.php', 'Accueil', $linkClass).
     nav_item('/contact.php', 'Contact', $linkClass);
 }
-?>
+
+
+function checkbox(string $name, string $value, array $data): string 
+{
+    $attribute = '';
+    if (isset($data[$name]) && in_array($value, $data[$name])){
+            $attribute .= 'checked';
+    }
+
+    return <<<HTML
+    <input type="checkbox" name="{$name}[]" value="$value" $attribute>
+HTML;
+}
+
+function radio(string $name, string $value, array $data): string 
+{
+    $attribute = '';
+    if (isset($data[$name]) && $value ===  $data[$name]){
+            $attribute .= 'checked';
+    }
+
+    return <<<HTML
+    <input type="radio" name="{$name}" value="$value" $attribute>
+HTML;
+}
+
+function dump ($variable){
+    echo '<pre>';
+    var_dump($variable);
+    echo '</pre>';
+}
